0a23861c9731cd86d9bbb7057e01e379e57583ba
Patotricks <patrickufrrj@gmail.com>
2025-02-19 15:29:52 -0300

    Adding comments

--- aws_sagemaker/app.py
+++ aws_sagemaker/app.py
@@ -8,18 +8,21 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# Get the dataset
 housing = fetch_california_housing(as_frame=True)
 df = housing.data
 df["target"] = housing.target 
 
+# Split into train and test
 train_data = df.sample(frac=0.8, random_state=42)
 test_data = df.drop(train_data.index)
 
+# Save s CSV
 os.makedirs("data", exist_ok=True)
 train_data.to_csv("data/train.csv", index=False, header=False)
 test_data.to_csv("data/test.csv", index=False, header=False)
 
+# Upload the train and test data to s3 bucket
 session = sagemaker.Session()
 role = os.getenv("GET_ROLE")
 bucket = session.default_bucket()
@@ -29,8 +32,10 @@ s3_train_path = session.upload_data(path="data/train.csv", bucket=bucket, key_pr
 s3_test_path = session.upload_data(path="data/test.csv", bucket=bucket, key_prefix=prefix)
 print(f"Dados enviados para S3: {s3_train_path}")
 
-xgb_container = sagemaker.image_uris.retrieve("xgboost", boto3.Session().region_name, "1.5-1")
+# Create the model
+xgb_container = sagemaker.image_uris.retrieve("xgboost", boto3.Session().region_name, "1.5-1") # Model container
 
+# Model estimator
 xgb_estimator = sagemaker.estimator.Estimator(
     image_uri=xgb_container,
     role=role,
@@ -40,16 +45,20 @@ xgb_estimator = sagemaker.estimator.Estimator(
     sagemaker_session=session
 )
 
+# Set hyperparameters
 xgb_estimator.set_hyperparameters(
     objective="reg:squarederror",
     num_round=20
 )
 
+# Set the trainining input
 train_input = TrainingInput(s3_train_path, content_type="csv")
 
+# Fit the model
 xgb_estimator.fit({"train": train_input})
 print("Modelo de regressão treinado com sucesso!")
 
+# Predict the new values
 predictor = xgb_estimator.deploy(
     initial_instance_count=1,
     instance_type="ml.m5.large"
--- aws_sagemaker/predict.py
+++ aws_sagemaker/predict.py
@@ -7,17 +7,21 @@ load_dotenv()
 
 endpoint_name = os.getenv("ENDPOINT_NAME")  
 
+# Create a SageMaker runtime client
 runtime = boto3.client("sagemaker-runtime")
 
+# Read the input values
 sample_input = np.array([[8.32, 0.1, 6.9, 2, 0.5, 9.3, 400, 18.5]])  
 
 payload = ",".join(map(str, sample_input.flatten()))
 
+# Invoke the endpoint
 response = runtime.invoke_endpoint(
     EndpointName=endpoint_name,
     ContentType="text/csv",
     Body=payload
 )
 
+# Print the result
 result = response["Body"].read().decode("utf-8")
 print(f"🎯 Previsão do modelo (preço da casa em milhares de dólares): {result}")
